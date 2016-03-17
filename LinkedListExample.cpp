25fad445bbd752898a8081684b7e63e6957b9d21
gokceyucel <gkcycl@gmail.com>
2016-03-17 19:01:23 +0200

    DLinkedList

--- LinkedListExample.cpp
+++ LinkedListExample.cpp
@@ -1,123 +1,123 @@
 //============================================================================
-// Name        : LinkedListExample.cpp
-// Author      : atc
+// Name        : dlinked_list.cpp
+// Author      : Gokce Yucel
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-
 #include <iostream>
-
 using namespace std;
 
-struct Node {
-	int data;
-	Node *next;
+struct DNode {
+    DNode *prev;
+    DNode *next;
+    int data;
 
-	Node(int d = 0) {
-		data = d;
-		next = NULL;
-	}
+public:
+    DNode(int d) {
+        data = d;
+        next = NULL;
+        prev = NULL;
+    }
 };
 
-class List {
-	Node* head;
-	unsigned int count;
+struct DList {
+    DNode *head;
+    DNode *tail;
+    int count;
+
 public:
-	List() {
-		head = NULL;
-		count = 0;
-	}
+    DList() {
+        head=NULL;
+        tail=NULL;
+        count=0;
+    }
 
-	void AddFront(int x) {
-		if (head == NULL) {
-			head = new Node(x);
-		}
-		else {
-			Node* tmp = head;
-			head = new Node(x);
-			head->next = tmp;
-		}
-		count++;
-	}
+    void AddFront(int x) {
+        if (head == NULL) {
+            head = tail = new DNode(x);
+        }
+        else {
+            DNode *tmp = head;
+            head = new DNode(x);
+            tmp->prev = head;
+            head->next = tmp;
+        }
+        count++;
+    }
 
-	void AddBack(int x) {
-		if (head == NULL) {
-			head = new Node(x);
-		}
-		else {
-			Node* p;
-			for (p = head; p->next != NULL; p = p->next);
+    void AddBack(int x) {
+        if (tail == NULL) {
+            tail = head = new DNode(x);
+        }
+        else {
+            DNode *tmp = tail;
+            tail = new DNode(x);
+            tmp->next = tail;
+            tail->prev = tmp;
+        }
+        count++;
+    }
 
-			p->next = new Node(x);
-		}
-		count++;
-	}
+    bool PopFront(int& ret) {
+        if (head == NULL) return false;
+        DNode* tmp = head;
+        head = tmp->next;
+        ret = tmp->data;
+        delete tmp;
+        count--;
+        return true;
+    }
 
-	bool PopFront(int& ret) {
-		if (head == NULL) return false;
-		Node* tmp = head;
-		head = tmp->next;
-		ret = tmp->data;
-		delete tmp;
-		count--;
-		return true;
-	}
+    bool PopBack(int& ret) {
+        if (tail == NULL) return false;
+        DNode* tmp = tail;
+        tail = tmp->prev;
+        ret = tmp->data;
+        delete tmp;
+        count--;
+        return true;
+    }
 
-	bool PopBack(int& ret) {
-		if (head == NULL) return false;
-		Node* p;
-		Node* prev = head;
-		for (p = head; p->next != NULL; p = p->next) prev = p;
-		ret = p->data;
-		if (prev == p) {
-			head = NULL;
-		}
-		else {
-			prev->next = NULL;
-		}
-		delete p;
-		count --;
-		return true;
-	}
+    DNode* GetElementAtPos(unsigned int pos) {
+        if (pos > count || head == NULL) return NULL;
+        unsigned int idx;
+        if (pos - count < pos) {
+            idx = count;
+            for (DNode* p = tail; p->prev != NULL; p = p->prev){
+                if (idx-- == pos) return p;
+            }
+        }
+        else {
+            idx = 0;
+            for (DNode* p = head; p->next != NULL; p = p->next){
+                if (++idx == pos) return p;
+            }
+        }
+        return NULL;
+    }
 
-	Node* GetElementAtPos(unsigned int pos) {
-		if (pos >= count) return NULL;
-		int idx = 0;
-		for (Node* p = head; p->next!=NULL; p=p->next)
-			if (idx++ == pos) return p;
-	}
-
-	void Print() {
-		cout << "List with " << count << " Elements:" << endl;
-		for (Node* p = head; p!= NULL; p = p->next) {
-			cout << p->data << " ";
-		}
-		cout << endl;
-	}
+    void Print() {
+        for (DNode* p = head; p != NULL; p = p->next) {
+            cout << p->data << " ";
+        }
+        cout << endl;
+    }
 };
 
 int main() {
-	List l;
+    DList dl;
+    dl.AddFront(1);
+    dl.AddFront(2);
+    dl.AddBack(3);
+    dl.AddFront(4);
+    dl.AddBack(9);
+    dl.Print();
 
-	l.AddFront(6);
-	l.AddFront(4);
-	l.AddBack(13);
-	l.AddFront(5);
-	l.AddFront(3);
-	l.AddBack(12);
+//  int ret;
+//  while(dl.PopBack(ret)) cout << ret << endl;
 
-	l.Print();
-
-	//int ret;
-	//while (l.PopBack(ret)) cout << ret << endl;
-
-	Node* p = l.GetElementAtPos(13);
-	if (p!=NULL)
-	cout << p->data << endl;
-	else cout << "Not Found" << endl;
-
-	l.Print();
-	return 0;
+    DNode* p = dl.GetElementAtPos(5);
+    cout << p->data << endl;
 }
